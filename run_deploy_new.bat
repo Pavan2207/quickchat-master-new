1dc0478c9ae88aa828517e63b6b56d39470230db
pavankumarkotni <pavankumarkotni2@gmail.com>
2026-02-27 23:24:53 +0530

    Delete run_deploy_new.bat

--- run_deploy_new.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d c:\Users\pavan\Downloads\quickchat-master\quickchat-master
-set CONVEX_DEPLOY_KEY=dev:astute-ostrich-773^|eyJ2MiI6IjVlYzkyYzgxMWIwYzQzYTZhODJlMWI2YzAxZjA2MzEwIn0=
-npx convex deploy
-pause

fcd76f23e3a5bb1a00817478cee79e243a57cf7d
pavankumarkotni <pavankumarkotni2@gmail.com>
2026-02-27 23:24:07 +0530

    Delete deploy_final.ps1

--- deploy_final.ps1
+++
@@ -1,3 +0,0 @@
-$env:CONVEX_DEPLOY_KEY="dev:astute-ostrich-773|eyJ2MiI6IjVlYzkyYzgxMWIwYzQzYTZhODJlMWI2YzAxZjA2MzEwIn0="
-Set-Location "c:\Users\pavan\Downloads\quickchat-master\quickchat-master"
-npx convex deploy

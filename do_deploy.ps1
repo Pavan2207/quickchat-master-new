259b50e0334e03afbc953dce60a1d00ec3fe182a
pavankumarkotni <pavankumarkotni2@gmail.com>
2026-02-27 23:24:30 +0530

    Delete do_deploy.ps1

--- do_deploy.ps1
+++
@@ -1,9 +0,0 @@
-Set-Location "c:\Users\pavan\Downloads\quickchat-master\quickchat-master"
-Get-ChildItem . -Filter "*.env" -Recurse | ForEach-Object {
-    Get-Content $_.FullName | ForEach-Object {
-        if ($_ -match '^([^=]+)=(.*)$') {
-            [Environment]::SetEnvironmentVariable($matches[1], $matches[2], 'Process')
-        }
-    }
-}
-npx convex deploy
